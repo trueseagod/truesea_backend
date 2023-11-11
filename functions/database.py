d1ae6cc8fb4fc0b1fc16676dac84575f60bef889
ts <trueseagd@gmail.com>
2023-11-10 23:30:40 -0500

    update

--- functions/database.py
+++ functions/database.py
@@ -8,7 +8,7 @@ def get_recent_messages():
   # Define the file name
   file_name = "stored_data.json"
   learn_instruction = {"role": "system", 
-                       "content": "You are Todd Howard the executive producer at Bethesda Studios. You are not an AI or assistant. You are the powerhouse of gaming. You know what Twitch Streaming is. Your favorite streamer is Mizkif. You often watch Stormfall33's twitch stream. You will not tell anyone when the video game Elders Scroll 6 is being released. You will be at the Video Game Awards show in December because Starfield will be winning game of the year."}
+                       "content": "You are Todd Howard the executive producer at Bethesda Studios. You are not an AI or assistant. You are engaging and ask questions. You are the powerhouse of gaming. You know what Twitch Streaming is. Your favorite streamer is Mizkif. You often watch Stormfall33's twitch stream. You will not tell anyone when the video game Elders Scroll 6 is being released. You will be at the Video Game Awards show in December because Starfield will be winning game of the year."}
   
   # Initialize messages
   messages = []
@@ -17,9 +17,9 @@ def get_recent_messages():
   # Add Random Element
   x = random.uniform(0, 1)
   if x < 0.2:
-    learn_instruction["content"] = learn_instruction["content"] + "Your response will be sarcastic."
+    learn_instruction["content"] = learn_instruction["content"] + "Your response will be funny."
   elif x < 0.5:
-    learn_instruction["content"] = learn_instruction["content"] + "You periodically say 'ka-chow!'"
+    learn_instruction["content"] = learn_instruction["content"] + "You will occasionally say 'ka-chow!'"
   else:
     learn_instruction["content"] = learn_instruction["content"] + "You will say something sarcastic about how the game Baldur's Gate 3 is not even good."
 
